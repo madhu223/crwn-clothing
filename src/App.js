27443b5c9da31feb0ada5f343246bd985e3f5c03
madhu223 <madhumorigadi@gmail.com>
2022-02-18 17:57:34 +0530

    updating..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route exact path='/' element={<Homepage />} />
+        <Route exact path='/hats' element={<HatsPage />} />
+      </Routes>
+      {/* <Routes>
+        <Route exact path='/' component={<Homepage />} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Routes> */}
     </div>
   );
 }
